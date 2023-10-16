1f19dee360fb77d372cd4157c722b943580c59fe
glburak <burakgul01@gmail.com>
2023-10-16 20:50:50 +0300

    syntax error

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,11 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+   <RouterView/>
 </template>
 
 <script>
-import jquery from '/src/vendors/jquery/dist/jquery.min.js'
-import bootstrap2 from '/src/vendors/bootstrap/dist/js/bootstrap.min.js'
-import fastclick from '/src/vendors/fastclick/lib/fastclick.js'
-import nprogress from '/src/vendors/nprogress/nprogress.js'
-import jqueryConcat from '/src/vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js'
-import custom from '/src/build/js/custom.min.js'
+
 
 export default {
   data(){
